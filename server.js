4a8cd150f4efb4d11bf21a47a7d18025a58a964b
narobinson <nathan.robinson327@gmail.com>
2021-10-19 13:15:50 -0500

    added app.use so routes can be accessed within main app

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ connectDB();
 app.get('/', (req, res) => res.send('API running'));
 
 //Define routes 
-//app.use('/api/users', require('./routes/api/users'));
-//app.use('/api/auth', require('./routes/api/auth'));
-//app.use('/api/profile', require('./routes/api/profile'));
-//app.use('/api/lists', require('./routes/api/lists'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/lists', require('./routes/api/lists'));
 
 const PORT = process.env.PORT || 5001;
 
