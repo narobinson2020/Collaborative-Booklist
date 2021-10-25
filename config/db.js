4b4d6f7cf1e48a2023e3c8c5bc26a9c8fb12a0d4
narobinson <nathan.robinson327@gmail.com>
2021-10-25 12:01:20 -0500

    removed useNewURLParser from file since it is no longer supported

--- config/db.js
+++ config/db.js
@@ -4,9 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(db, {
-          useNewURLParser: true
-      });
+      await mongoose.connect(db);
 
       console.log('MongoDB connected...')
     } catch(err) {
