73859de788e8c93dec63bec1b9be1056052274ca
narobinson <nathan.robinson327@gmail.com>
2022-04-12 07:56:59 -0500

    issue with route component not rendering css has been solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './Components/Layout/Auth/SignIn';
 import SignUp from './Components/Layout/Auth/SignUp';
-import Authentication from './Components/Layout/Auth/Authentication';
+import Landing from './Components/Layout/Auth/Landing';
 
-const App = () => 
-    <Fragment >
-        <Authentication />
+const App = () => (
+  <Router>
+    <Fragment>
+        {/* Components that you want to render regardless of the page (ex: nav bar) should be put outside the routes component but inside the router component */}
+        <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/signup" element={<SignUp />} />
+            <Route exact path="/signin" element={<SignIn />} />
+        </Routes>
+        
     </Fragment>
+  </Router>
+);
 
 export default App;
---
+++ client/src/Components/Layout/Auth/Landing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <div className='title-auth'>
+        <h1>Welcome to Your Booklist!</h1>
+      </div>
+      <div className='container-auth'>
+        <div className='register-auth'>
+          <button>
+            <a href='SignUp.html'>Sign Up</a>
+          </button>
+        </div>
+        <div className='login-auth'>
+          <button>
+            <a href='SignIn.html'>Sign In</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
