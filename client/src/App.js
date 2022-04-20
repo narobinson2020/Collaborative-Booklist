3e73e589d2f0da27c4a58e3eeacbc1ce1a88b52a
narobinson <nathan.robinson327@gmail.com>
2022-04-19 20:44:39 -0500

    saving changes to app.js and dependecies in package.json file

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,26 @@ import SignIn from './Components/Layout/Auth/SignIn';
 import SignUp from './Components/Layout/Auth/SignUp';
 import Landing from './Components/Layout/Auth/Landing';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+// App doesn't render again. Check what changed after adding useState and hook videos 
+
 const App = () => (
-  <Router>
-    <Fragment>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
         {/* Components that you want to render regardless of the page (ex: nav bar) should be put outside the routes component but inside the router component */}
         <Routes>
-            <Route exact path="/" element={<Landing />} />
-            <Route exact path="/signup" element={<SignUp />} />
-            <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path='/' element={<Landing />} />
+          <Route exact path='/signup' element={<SignUp />} />
+          <Route exact path='/signin' element={<SignIn />} />
         </Routes>
-        
-    </Fragment>
-  </Router>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
